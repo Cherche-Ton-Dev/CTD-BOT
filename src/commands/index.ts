cb0b562f6b48a8c0c4ac1999402fe40640917517
Rafael <rafael.martinezcalvo@orange.fr>
2021-09-29 16:06:20 +0200

    remove example command
    not needed because there is already true command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,7 @@
 import { ICommandList } from "../types/commands.js";
 
-import * as example from "./example.js";
 import * as suggest from "./suggest.js";
-// import * as autreCommande from "./autreCommande";
 
 export const commands: ICommandList = {
-    example,
     suggest,
-    //  autreCommande
 };
