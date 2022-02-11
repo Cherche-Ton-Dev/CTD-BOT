211fb24d9765662d84000d7a918136738bcd3895
Rafael <rafael.martinezcalvo@orange.fr>
2022-02-11 18:24:13 +0100

    migrate config

--- src/context/config.ts
+++ src/context/config.ts
@@ -1,21 +1,22 @@
 import { Dev } from "../types/missions";
 
 const rawConfig = {
-    suggestionsChanelId: "856601907346145281",
-    pendingChannelId: "902632260451319808",
-    modoRoleId: "860881358721253378",
+    suggestionsChanelId: "934916135491608702",
+    pendingChannelId: "934916135051198471",
+    modoRoleId: "934916134329786383",
+    ticketCategoryId: "934916136259182603",
     missionChannelIDS: {
-        "web-dev": {
-            free: "902601841710334062",
-            payed: "902601721220587560",
+        dev: {
+            free: "934916135709724706",
+            payed: "934916135709724707",
         },
         artist: {
-            free: "876441952445431889",
-            payed: "876442066283024384",
+            free: "934916135709724708",
+            payed: "934916135709724709",
         },
-        dev: {
-            free: "856612483443064852",
-            payed: "876441721515433994",
+        "web-dev": {
+            free: "934916135709724710",
+            payed: "934916135709724711",
         },
     } as { [key in Dev]: { free: string; payed: string } },
     devRoles: [
@@ -24,60 +25,23 @@ const rawConfig = {
             value: "dev" as Dev,
             emoji: "👨‍💻",
             description: "Pour créer un bot discord, programme, plugin, etc.",
-            roleID: "856624575684673566",
+            roleID: "934916134329786378",
         },
         {
             label: "Artiste",
             value: "artist" as Dev,
             emoji: "🎨",
             description: "Pour créer un design de site web, une bannière, etc.",
-            roleID: "858310927447097374",
+            roleID: "934916134313033855",
         },
         {
             label: "Dev Web",
             value: "web-dev" as Dev,
             emoji: "🌐",
             description: "Pour créer un site web.",
-            roleID: "856624575684673566",
-        },
-    ],
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const testConfig = {
-    ...rawConfig,
-    suggestionsChanelId: "892774559147782154",
-    pendingChannelId: "897503615357427732",
-    modoRoleId: "905389455765037116",
-    missionChannelIDS: {
-        "web-dev": {
-            free: "902124287828312104",
-            payed: "902124318530609193",
-        },
-        artist: {
-            free: "902124492766216203",
-            payed: "902124525024591912",
-        },
-        dev: {
-            free: "902124390458728449",
-            payed: "902124415934922772",
-        },
-    } as { [key in Dev]: { free: string; payed: string } },
-    devRoles: [
-        {
-            ...rawConfig.devRoles[0],
-            roleID: "902132191834804264",
-        },
-        {
-            ...rawConfig.devRoles[1],
-            roleID: "902132141821919272",
-        },
-        {
-            ...rawConfig.devRoles[2],
-            roleID: "902132166773833748",
+            roleID: "934916134313033856",
         },
     ],
 };
 
 export const config = rawConfig;
-// export const config = testConfig;
