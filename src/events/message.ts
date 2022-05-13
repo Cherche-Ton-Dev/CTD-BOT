25ed18f4f0d553e01a7a2d10f11e59222bda106f
Rafael <rafael.martinezcalvo@orange.fr>
2022-05-13 11:17:01 +0200

    add bump contribution points

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,20 +1,34 @@
 import { GuildMember, Message } from "discord.js";
 import { addPoints } from "../db/api/member";
-import { messagePoints } from "../utils/equations";
+import { bumpPoints, messagePoints } from "../utils/equations";
 
 const lastMessages: Map<string, Date> = new Map();
 
 export async function handleMessageCreated(message: Message) {
-    if (message.author.bot || !(message.member instanceof GuildMember)) {
+    if (!(message.member instanceof GuildMember)) {
         return;
     }
 
-    const lastMessageDate = lastMessages.get(message.member.id);
+    await points(message.member);
+    if (
+        message.author.id === "302050872383242240" /*disboard*/ &&
+        message.interaction?.commandName === "bump"
+    ) {
+        const contribPoints = bumpPoints();
+        await addPoints(message.member, contribPoints);
+        await message.channel.send(
+            `${message.interaction.user} a reçu ${contribPoints} points de contribution pour son bump 👍.`,
+        );
+    }
+}
+
+async function points(member: GuildMember) {
+    const lastMessageDate = lastMessages.get(member.id);
     if (lastMessageDate && lastMessageDate.getTime() + 1000 * 10 > Date.now()) {
         return;
     }
 
-    lastMessages.set(message.member.id, new Date());
+    lastMessages.set(member.id, new Date());
     const contribPoints = messagePoints();
-    await addPoints(message.member, contribPoints);
+    await addPoints(member, contribPoints);
 }
