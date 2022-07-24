172705097a61560fdea83b7708e7211e18fd72b7
rafalou38 <rafael.martinezcalvo@orange.fr>
2022-07-24 13:58:15 +0200

    5 bump points for adrix 😈

--- src/utils/equations.ts
+++ src/utils/equations.ts
@@ -1,4 +1,7 @@
+import { GuildMember } from "discord.js";
+
 export const ratingPoints = (rating: number) => 35 * rating - 60;
 export const messagePoints = () => 0.75;
-export const bumpPoints = () => 10;
+export const bumpPoints = (member: GuildMember) =>
+    member.id == "778182909119037441" ? 5 : 10;
 export const invitePoints = () => 15;
