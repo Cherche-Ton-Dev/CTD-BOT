cdfcb66a8e8646bcf9cff1dc9ca0bfb398b63e8e
Rafael <rafael.martinezcalvo@orange.fr>
2023-04-07 21:54:18 +0200

    fix bug mp bloquées

--- src/buttons/create-mission.ts
+++ src/buttons/create-mission.ts
@@ -13,42 +13,55 @@ export async function run(
     client: Client,
     interaction: ButtonInteraction,
 ): Promise<CommandReturn> {
-    const DM = await interaction.user.createDM();
-    const sentMessage = await DM.send(
-        "** **\n\n\n\n\n\n\n\n\n\n\n\nCreation d'une nouvelle mission.\n❗ Tu possèdes désormais **5 minutes** pour répondre à chaque question ❗",
-    );
+    try {
+        const DM = await interaction.user.createDM();
+        const sentMessage = await DM.send(
+            "** **\n\n\n\n\n\n\n\n\n\n\n\nCreation d'une nouvelle mission.\n❗ Tu possèdes désormais **5 minutes** pour répondre à chaque question ❗",
+        );
 
-    createMission(DM, interaction.member as GuildMember); // not awaited: will run separated
+        createMission(DM, interaction.member as GuildMember); // not awaited: will run separated
 
-    await interaction.reply({
-        embeds: [
-            {
-                title: "Nouvelle Mission",
-                description:
-                    "Fais un tour dans tes messages privés pour nous donner les informations nécessaire à la création de la mission !",
-                color: "GREEN",
-                thumbnail: { url: client.user?.avatarURL() || "" },
-                url: sentMessage.url,
-            },
-        ],
-        components: [
-            {
-                type: "ACTION_ROW",
-                components: [
-                    {
-                        type: "BUTTON",
-                        style: "LINK",
-                        label: "Messages Privées",
-                        url: sentMessage.url,
-                    },
-                ],
-            },
-        ],
-        ephemeral: true,
-    });
+        await interaction.reply({
+            embeds: [
+                {
+                    title: "Nouvelle Mission",
+                    description:
+                        "Fais un tour dans tes messages privés pour nous donner les informations nécessaire à la création de la mission !",
+                    color: "GREEN",
+                    thumbnail: { url: client.user?.avatarURL() || "" },
+                    url: sentMessage.url,
+                },
+            ],
+            components: [
+                {
+                    type: "ACTION_ROW",
+                    components: [
+                        {
+                            type: "BUTTON",
+                            style: "LINK",
+                            label: "Messages Privées",
+                            url: sentMessage.url,
+                        },
+                    ],
+                },
+            ],
+            ephemeral: true,
+        });
 
-    return {
-        status: "OK",
-        label: "succès",
-    };
+        return {
+            status: "OK",
+            label: "succès",
+        };
+
+    } catch {
+        interaction.reply({
+            ephemeral: true,
+            content: "Merci d'ouvrir tes MPs pour remplir ta mission"
+        });
+
+        return {
+            status: "ERROR",
+            label: "NO MP",
+        };
+    }
 }
