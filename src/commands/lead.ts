1d92a4b2e33655300ed63194edaa5bdcc4c9b092
Rafael <rafael.martinezcalvo@orange.fr>
2023-08-18 14:27:34 +0200

    ran prettier

--- src/commands/lead.ts
+++ src/commands/lead.ts
@@ -43,7 +43,7 @@ export async function run(
                 interaction.guild?.members
                     .fetch(member.discordID)
                     // eslint-disable-next-line @typescript-eslint/no-empty-function
-                    .catch(() => { })
+                    .catch(() => {})
             );
         }),
     );
